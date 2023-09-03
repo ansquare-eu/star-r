f28ce01920cbbebad2659d972fa7c80b20250290
farpo <77583105+farpo@users.noreply.github.com>
2023-09-03 09:21:13 +0200

    power keys

--- src/main/java/eu/ansquare/starr/client/ModKeyBinds.java
+++ src/main/java/eu/ansquare/starr/client/ModKeyBinds.java
@@ -15,6 +15,16 @@ import org.quiltmc.qsl.networking.api.client.ClientPlayNetworking;
 public class ModKeyBinds {
 	private static KeyBind transformPowerKey;
 	private static KeyBind firstPowerKey;
+	private static KeyBind secondPowerKey;
+	private static KeyBind thirdPowerKey;
+	private static KeyBind fourthPowerKey;
+	private static KeyBind fifthPowerKey;
+	private static KeyBind sixthPowerKey;
+	private static KeyBind seventhPowerKey;
+	private static KeyBind eightPowerKey;
+	private static KeyBind ninthPowerKey;
+	private static KeyBind tenthPowerKey;
+
 	public static void init(){
 		transformPowerKey = KeyBindingHelper.registerKeyBinding(new KeyBind(
 				"key.starr.transformation", // The translation key of the keybinding's name
@@ -36,6 +46,105 @@ public class ModKeyBinds {
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if (firstPowerKey.wasPressed()) {
 				ClientPlayNetworking.send(ModPackets.POWER_PACKET_ID, PacketByteBufs.create().writeEnumConstant(PowerOrder.FIRST));
+			}
+		});
+		secondPowerKey = KeyBindingHelper.registerKeyBinding(new KeyBind(
+				"key.starr.secondpower", // The translation key of the keybinding's name
+				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_K, // The keycode of the key
+				"category.starr.powers" // The translation key of the keybinding's category.
+		));
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (secondPowerKey.wasPressed()) {
+				ClientPlayNetworking.send(ModPackets.POWER_PACKET_ID, PacketByteBufs.create().writeEnumConstant(PowerOrder.SECOND));
+			}
+		});
+		thirdPowerKey = KeyBindingHelper.registerKeyBinding(new KeyBind(
+				"key.starr.thirdpower", // The translation key of the keybinding's name
+				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_M, // The keycode of the key
+				"category.starr.powers" // The translation key of the keybinding's category.
+		));
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (thirdPowerKey.wasPressed()) {
+				ClientPlayNetworking.send(ModPackets.POWER_PACKET_ID, PacketByteBufs.create().writeEnumConstant(PowerOrder.THIRD));
+			}
+		});
+		fourthPowerKey = KeyBindingHelper.registerKeyBinding(new KeyBind(
+				"key.starr.fourthpower", // The translation key of the keybinding's name
+				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_N, // The keycode of the key
+				"category.starr.powers" // The translation key of the keybinding's category.
+		));
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (fourthPowerKey.wasPressed()) {
+				ClientPlayNetworking.send(ModPackets.POWER_PACKET_ID, PacketByteBufs.create().writeEnumConstant(PowerOrder.FOURTH));
+			}
+		});
+		fifthPowerKey = KeyBindingHelper.registerKeyBinding(new KeyBind(
+				"key.starr.fifthpower", // The translation key of the keybinding's name
+				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_B, // The keycode of the key
+				"category.starr.powers" // The translation key of the keybinding's category.
+		));
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (fifthPowerKey.wasPressed()) {
+				ClientPlayNetworking.send(ModPackets.POWER_PACKET_ID, PacketByteBufs.create().writeEnumConstant(PowerOrder.FIFTH));
+			}
+		});
+		sixthPowerKey = KeyBindingHelper.registerKeyBinding(new KeyBind(
+				"key.starr.sixthpower", // The translation key of the keybinding's name
+				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_J, // The keycode of the key
+				"category.starr.powers" // The translation key of the keybinding's category.
+		));
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (sixthPowerKey.wasPressed()) {
+				ClientPlayNetworking.send(ModPackets.POWER_PACKET_ID, PacketByteBufs.create().writeEnumConstant(PowerOrder.SIXTH));
+			}
+		});
+		seventhPowerKey = KeyBindingHelper.registerKeyBinding(new KeyBind(
+				"key.starr.seventhpower", // The translation key of the keybinding's name
+				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_H, // The keycode of the key
+				"category.starr.powers" // The translation key of the keybinding's category.
+		));
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (seventhPowerKey.wasPressed()) {
+				ClientPlayNetworking.send(ModPackets.POWER_PACKET_ID, PacketByteBufs.create().writeEnumConstant(PowerOrder.SEVENTH));
+			}
+		});
+		eightPowerKey = KeyBindingHelper.registerKeyBinding(new KeyBind(
+				"key.starr.eightpower", // The translation key of the keybinding's name
+				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_Y, // The keycode of the key
+				"category.starr.powers" // The translation key of the keybinding's category.
+		));
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (eightPowerKey.wasPressed()) {
+				ClientPlayNetworking.send(ModPackets.POWER_PACKET_ID, PacketByteBufs.create().writeEnumConstant(PowerOrder.EIGHT));
+			}
+		});
+		ninthPowerKey = KeyBindingHelper.registerKeyBinding(new KeyBind(
+				"key.starr.ninthpower", // The translation key of the keybinding's name
+				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_V, // The keycode of the key
+				"category.starr.powers" // The translation key of the keybinding's category.
+		));
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (ninthPowerKey.wasPressed()) {
+				ClientPlayNetworking.send(ModPackets.POWER_PACKET_ID, PacketByteBufs.create().writeEnumConstant(PowerOrder.NINHT));
+			}
+		});
+		tenthPowerKey = KeyBindingHelper.registerKeyBinding(new KeyBind(
+				"key.starr.tenthpower", // The translation key of the keybinding's name
+				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_APOSTROPHE, // The keycode of the key
+				"category.starr.powers" // The translation key of the keybinding's category.
+		));
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (tenthPowerKey.wasPressed()) {
+				ClientPlayNetworking.send(ModPackets.POWER_PACKET_ID, PacketByteBufs.create().writeEnumConstant(PowerOrder.TENTH));
 			}
 		});
 	}
