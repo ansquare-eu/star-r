78ddfd5472c0fbf503ff98a44cfcd5ec87548522
farpo <77583105+farpo@users.noreply.github.com>
2023-09-21 07:27:26 +0200

    good progresss

---
+++ src/main/java/eu/ansquare/starr/util/network/ClientLaser.java
@@ -0,0 +1,12 @@
+package eu.ansquare.starr.util.network;
+
+import net.minecraft.util.Pair;
+
+import java.awt.*;
+
+public class ClientLaser {
+	public Color COLOR;
+	public ClientLaser(int color){
+		this.COLOR = new Color(color);
+	}
+}
