33e639b95bec7f172014864ca94737fef0d21032
farpo <77583105+farpo@users.noreply.github.com>
2023-10-08 15:47:13 +0200

    forgot to mark files

--- src/main/java/eu/ansquare/starr/screenhandler/TeleportScreenHandler.java
+++ src/main/java/eu/ansquare/starr/screenhandler/TeleportScreenHandler.java
@@ -1,23 +1,49 @@
 package eu.ansquare.starr.screenhandler;
 
+import eu.ansquare.starr.entity.PalicaEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
-public class TeleportScreenHandler extends ScreenHandler {
-	protected TeleportScreenHandler(@Nullable ScreenHandlerType<?> type, int syncId) {
-		super(type, syncId);
-	}
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
 
-	@Override
-	public ItemStack quickTransfer(PlayerEntity player, int fromIndex) {
-		return null;
+public class TeleportScreenHandler extends ScreenHandler implements Supplier<Map<Integer, Slot>> {
+	public final static HashMap<String, Object> guistate = new HashMap<>();
+	public final World world;
+	public final PlayerEntity entity;
+	public int x, y, z;
+	//private IItemHandler internal;
+	private final Map<Integer, Slot> customSlots = new HashMap<>();
+	private boolean bound = false;
+
+	public TeleportScreenHandler(int id, PlayerInventory inv) {
+		super(ModScreenHandlers.TELEPORT_SCREEN, id);
+		this.entity = inv.player;
+		this.world = inv.player.getWorld();
+		//this.internal = new ItemStackHandler(0);
 	}
 
 	@Override
 	public boolean canUse(PlayerEntity player) {
-		return false;
+		return true;
+	}
+
+	@Override
+	public ItemStack quickTransfer(PlayerEntity playerIn, int index) {
+		return ItemStack.EMPTY;
+	}
+
+	public Map<Integer, Slot> get() {
+		return customSlots;
 	}
 }
