dfcabc4a01d5024e23c2489dedce5629b745b5cb
farpo <77583105+farpo@users.noreply.github.com>
2023-10-08 18:14:12 +0200

    transformation

--- src/main/java/eu/ansquare/starr/power/TestTransformationPower.java
+++ src/main/java/eu/ansquare/starr/power/TestTransformationPower.java
@@ -1,7 +1,10 @@
 package eu.ansquare.starr.power;
 
+import eu.ansquare.starr.entity.ModEntities;
+import eu.ansquare.starr.entity.PalicaEntity;
 import eu.ansquare.starr.util.datasaving.IDataSaver;
 import net.fabricmc.fabric.api.entity.FakePlayer;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 
@@ -13,6 +16,12 @@ public class TestTransformationPower extends Power{
 
 	@Override
 	public void onActivate(ServerPlayerEntity player) {
-
+		PalicaEntity entity = (PalicaEntity) ModEntities.PALICA.create(player.getWorld());
+		entity.setOwner(player);
+		entity.setPosition(player.getEyePos().add(player.getRotationVec(1).normalize().multiply(5)));
+		entity.setNoGravity(true);
+		entity.pickupType = PersistentProjectileEntity.PickupPermission.ALLOWED;
+		entity.retrieve();
+		player.getWorld().spawnEntity(entity);
 	}
 }
