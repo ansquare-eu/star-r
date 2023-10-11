adea749e400c90eacb5b8020cb3f375c7c13b00d
farpo <77583105+farpo@users.noreply.github.com>
2023-10-11 20:54:54 +0200

    ooh nice screens you got there

--- src/main/java/eu/ansquare/starr/items/testing/GetSuperTypeTesterItem.java
+++ src/main/java/eu/ansquare/starr/items/testing/GetSuperTypeTesterItem.java
@@ -1,9 +1,14 @@
 package eu.ansquare.starr.items.testing;
 
 import eu.ansquare.starr.cca.StarREntityComponents;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -20,6 +25,7 @@ public class GetSuperTypeTesterItem extends Item {
 				String message = superDudeComponent.getType().queryMessage();
 				user.sendMessage(Text.literal(message), false);
 			});
+			user.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId, playerInventory, playerx) -> ScreenHandlerType.GENERIC_9X3.create(syncId, playerInventory), Text.literal("meme")));
 
 		}
 		return TypedActionResult.success(user.getStackInHand(hand));
