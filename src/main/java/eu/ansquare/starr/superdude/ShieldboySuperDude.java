485be677fa8be4b0b33ace8dffb5a9e675e962eb
farpo <77583105+farpo@users.noreply.github.com>
2023-11-06 18:10:02 +0100

    the three turtle powers

--- src/main/java/eu/ansquare/starr/superdude/ShieldboySuperDude.java
+++ src/main/java/eu/ansquare/starr/superdude/ShieldboySuperDude.java
@@ -3,7 +3,14 @@ package eu.ansquare.starr.superdude;
 import eu.ansquare.starr.items.ModItems;
 import eu.ansquare.starr.power.Powers;
 import eu.ansquare.starr.power.creation.CreateItemPower;
+import eu.ansquare.starr.power.creation.SummonEntityPower;
 import eu.ansquare.starr.util.power.FlightType;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.FallingBlockEntity;
+import net.minecraft.entity.passive.TurtleEntity;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
 
 import java.awt.*;
 
@@ -16,8 +23,31 @@ public class ShieldboySuperDude extends SuperDude{
 	public void initPowers() {
 		addPower(PowerOrder.FIRST, Powers.FORCEFIELD_POWER);
 		addPower(PowerOrder.SECOND, new CreateItemPower(ModItems.FORCESWORD, 1, true));
+		addPower(PowerOrder.THIRD, new SummonEntityPower((player) -> {
+			Vec3d playerDir = player.getRotationVec(1f).normalize().multiply(64);
+			BlockHitResult blockHitResult = player.getWorld().raycast(new RaycastContext(player.getEyePos(), player.getEyePos().add(playerDir), RaycastContext.ShapeType.VISUAL, RaycastContext.FluidHandling.NONE, player));
+			if (blockHitResult != null) {
+				TurtleEntity turtle = EntityType.TURTLE.create(player.getServerWorld());
+				turtle.setPosition(blockHitResult.getPos());
+				turtle.setBaby(true);
+				player.getServerWorld().spawnEntity(turtle);
+			}
+		}));
+		addPower(PowerOrder.FOURTH, new SummonEntityPower((player) -> {
+			Vec3d playerDir = player.getRotationVec(1f).normalize().multiply(64);
+			BlockHitResult blockHitResult = player.getWorld().raycast(new RaycastContext(player.getEyePos(), player.getEyePos().add(playerDir), RaycastContext.ShapeType.VISUAL, RaycastContext.FluidHandling.NONE, player));
+			if (blockHitResult != null) {
+				TurtleEntity turtle = EntityType.TURTLE.create(player.getServerWorld());
+				turtle.setPosition(blockHitResult.getPos());
+				player.getServerWorld().spawnEntity(turtle);
+			}
+		}));
+		addPower(PowerOrder.FIFTH, new SummonEntityPower((player) -> {
+			TurtleEntity turtle = EntityType.TURTLE.create(player.getServerWorld());
+			turtle.startRiding(player);
+			player.getServerWorld().spawnEntity(turtle);
+		}));
 	}
-
 	@Override
 	public void initModifiers() {
 
