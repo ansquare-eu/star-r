c858eceabf0e180bfc0aea0d167e2d4528737853
farpo <77583105+farpo@users.noreply.github.com>
2023-11-26 08:39:23 +0100

    respawning fixed after 2 months

--- src/main/java/eu/ansquare/starr/cca/StarREntityComponents.java
+++ src/main/java/eu/ansquare/starr/cca/StarREntityComponents.java
@@ -4,6 +4,7 @@ import dev.onyxstudios.cca.api.v3.component.ComponentKey;
 import dev.onyxstudios.cca.api.v3.component.ComponentRegistryV3;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
+import dev.onyxstudios.cca.api.v3.entity.RespawnCopyStrategy;
 import eu.ansquare.starr.StarR;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.Identifier;
@@ -16,6 +17,9 @@ public final class StarREntityComponents implements EntityComponentInitializer {
 	@Override
 	public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
 		registry.registerFor(LivingEntity.class, SUPER_DUDE_COMPONENT, world -> new SuperDudeComponent());
+		registry.registerForPlayers(SUPER_DUDE_COMPONENT, player -> new SuperDudeComponent(), RespawnCopyStrategy.ALWAYS_COPY);
 		registry.registerFor(LivingEntity.class, TELEPORT_LOC_COMPONENT, world -> new TeleportLocComponent());
+		registry.registerForPlayers(TELEPORT_LOC_COMPONENT, player -> new TeleportLocComponent(), RespawnCopyStrategy.ALWAYS_COPY);
+
 	}
 }
