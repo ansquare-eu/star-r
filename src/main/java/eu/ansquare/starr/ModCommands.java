2ab33a8a51527e6e2acb2ce8188821466ac3aa17
farpo <77583105+farpo@users.noreply.github.com>
2023-12-02 19:34:37 +0100

    quotes quotes no more quotes

--- src/main/java/eu/ansquare/starr/ModCommands.java
+++ src/main/java/eu/ansquare/starr/ModCommands.java
@@ -2,7 +2,9 @@ package eu.ansquare.starr;
 import eu.ansquare.starr.superdude.SuperDude;
 import eu.ansquare.starr.superdude.SuperDudes;
 import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.command.argument.ResourceKeyArgument;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
 import org.quiltmc.qsl.command.api.CommandRegistrationCallback;
 
@@ -16,9 +18,9 @@ public class ModCommands {
 	public static void init(){
 		CommandRegistrationCallback.EVENT.register((dispatcher, buildContext, environment) -> dispatcher.register(literal("superdude")
 				.then(argument("target", player())
-						.then(argument("id", string()).requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(4))
+						.then(argument("id", ResourceKeyArgument.key(ModRegistries.SUPER_DUDES.getKey())).requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(4))
 								.executes(context -> {
-									SuperDude superDude = SuperDudes.getSuperDude(new Identifier(context.getArgument("id", String.class)));
+									SuperDude superDude = SuperDudes.getSuperDude(context.getArgument("id", RegistryKey.class).getValue());
 									PlayerEntity player = EntityArgumentType.getPlayer(context, "target");
 									SuperDudes.applyToPlayer(player, superDude);
 									return 1;
