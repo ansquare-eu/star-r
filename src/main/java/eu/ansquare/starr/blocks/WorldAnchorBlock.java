08566e759bfd2be11dad1a28bf0ca54db1562274
farpo <77583105+farpo@users.noreply.github.com>
2023-12-04 10:18:31 +0100

    tested the laser offsets and cleanup

--- src/main/java/eu/ansquare/starr/blocks/WorldAnchorBlock.java
+++ src/main/java/eu/ansquare/starr/blocks/WorldAnchorBlock.java
@@ -2,12 +2,8 @@ package eu.ansquare.starr.blocks;
 
 import eu.ansquare.starr.util.math.Math3D;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockWithEntity;
-import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
 
 public class WorldAnchorBlock extends Block {
@@ -18,7 +14,7 @@ public class WorldAnchorBlock extends Block {
 	}
 	public static boolean isInVicinity(boolean spatial, BlockPos loc, World world){
 		Box box = new Box(loc).expand(8);
-		return Math3D.predicateBoxIterator(box, (blockPos -> {
+		return Math3D.boxSearch(box, (blockPos -> {
 			if(world.getBlockState(blockPos).getBlock() instanceof WorldAnchorBlock block){
 				return block.isSpatial == spatial;
 			}
