624a903d1d4ad85a9163c84dd4e3290e97b13ccd
farpo <77583105+farpo@users.noreply.github.com>
2023-12-29 10:14:01 +0100

    finally apoli set up

--- src/main/java/eu/ansquare/squarepowered/Squarepowered.java
+++ src/main/java/eu/ansquare/squarepowered/Squarepowered.java
@@ -1,11 +1,21 @@
 package eu.ansquare.squarepowered;
 
+import eu.ansquare.starr.StarR;
+import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Squarepowered implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("Squarepowered");
+	public static final String MODID = StarR.MODID;
+	public static Identifier id(String path){
+		return StarR.id(path);
+	}
 	@Override
 	public void onInitialize(ModContainer mod) {
-
+		LOGGER.info("Hello Quilt world from Squarepowered lib!");
+		LOGGER.info("Squarepowered lib successfully initialized!");
 	}
 }
---
+++ src/main/java/eu/ansquare/squarepowered/client/SquarepoweredClient.java
@@ -0,0 +1,53 @@
+package eu.ansquare.squarepowered.client;
+
+import com.mojang.blaze3d.platform.InputUtil;
+import io.github.apace100.apoli.ApoliClient;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBind;
+import org.lwjgl.glfw.GLFW;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+
+public class SquarepoweredClient implements ClientModInitializer {
+	public static KeyBind powerKeybind3;
+	public static KeyBind powerKeybind4;
+	public static KeyBind powerKeybind5;
+	public static KeyBind powerKeybind6;
+	public static KeyBind powerKeybind7;
+	public static KeyBind powerKeybind8;
+	public static KeyBind powerKeybind9;
+	public static KeyBind powerKeybind10;
+	public static KeyBind powerKeybindTransform;
+	@Override
+	public void onInitializeClient(ModContainer mod) {
+		powerKeybind3 = new KeyBind("key.origins.3_active", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_M, "category.origins");
+		powerKeybind4 = new KeyBind("key.origins.4_active", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_N, "category.origins");
+		powerKeybind5 = new KeyBind("key.origins.5_active", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_B, "category.origins");
+		powerKeybind6 = new KeyBind("key.origins.6_active", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_I, "category.origins");
+		powerKeybind7 = new KeyBind("key.origins.7_active", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_H, "category.origins");
+		powerKeybind8 = new KeyBind("key.origins.8_active", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_Y, "category.origins");
+		powerKeybind9 = new KeyBind("key.origins.9_active", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_V, "category.origins");
+		powerKeybind10 = new KeyBind("key.origins.10_active", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_Z, "category.origins");
+		powerKeybindTransform = new KeyBind("key.origins.11_active", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_MINUS, "category.origins");
+		ApoliClient.registerPowerKeybinding("key.origins.3_active", powerKeybind3);
+		ApoliClient.registerPowerKeybinding("key.origins.4_active", powerKeybind4);
+		ApoliClient.registerPowerKeybinding("key.origins.5_active", powerKeybind5);
+		ApoliClient.registerPowerKeybinding("key.origins.6_active", powerKeybind6);
+		ApoliClient.registerPowerKeybinding("key.origins.7_active", powerKeybind7);
+		ApoliClient.registerPowerKeybinding("key.origins.8_active", powerKeybind8);
+		ApoliClient.registerPowerKeybinding("key.origins.9_active", powerKeybind9);
+		ApoliClient.registerPowerKeybinding("key.origins.10_active", powerKeybind10);
+		ApoliClient.registerPowerKeybinding("key.origins.11_active", powerKeybindTransform);
+		KeyBindingHelper.registerKeyBinding(powerKeybind3);
+		KeyBindingHelper.registerKeyBinding(powerKeybind4);
+		KeyBindingHelper.registerKeyBinding(powerKeybind5);
+		KeyBindingHelper.registerKeyBinding(powerKeybind6);
+		KeyBindingHelper.registerKeyBinding(powerKeybind7);
+		KeyBindingHelper.registerKeyBinding(powerKeybind8);
+		KeyBindingHelper.registerKeyBinding(powerKeybind9);
+		KeyBindingHelper.registerKeyBinding(powerKeybind10);
+		KeyBindingHelper.registerKeyBinding(powerKeybindTransform);
+
+
+	}
+}
