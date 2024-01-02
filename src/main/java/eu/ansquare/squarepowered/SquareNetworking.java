690a854378cdcbf4a587347dc431dc9f6fbc4ad7
farpo <77583105+farpo@users.noreply.github.com>
2024-01-02 19:48:32 +0100

    fixed the errors

--- src/main/java/eu/ansquare/squarepowered/SquareNetworking.java
+++ src/main/java/eu/ansquare/squarepowered/SquareNetworking.java
@@ -1,7 +1,12 @@
 package eu.ansquare.squarepowered;
 
+import eu.ansquare.squarepowered.actionscreen.ActionScreenC2SPacket;
+import net.minecraft.util.Identifier;
+import org.quiltmc.qsl.networking.api.ServerPlayNetworking;
+
 public class SquareNetworking {
+	public static Identifier ACTION_SCREEN_PACKET = Squarepowered.id("action_screen_packet");
 	public static void initC2S(){
-		ServerPlayNetworking.registerGlobalReceiver(AS_PACKET_ID, ASPacket::receive);
+		ServerPlayNetworking.registerGlobalReceiver(ACTION_SCREEN_PACKET, ActionScreenC2SPacket::receive);
 	}
 }
