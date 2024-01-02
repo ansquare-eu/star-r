a7e5ee5dbd0371d0ea16d8ae4e55ef7e3e52e459
farpo <77583105+farpo@users.noreply.github.com>
2024-01-02 09:48:50 +0100

    telekinesis has block

--- src/main/java/eu/ansquare/squarepowered/action/bientity/TelekinesActions.java
+++ src/main/java/eu/ansquare/squarepowered/action/bientity/TelekinesActions.java
@@ -7,9 +7,15 @@ import io.github.apace100.apoli.power.factory.action.ActionFactory;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.apace100.calio.data.SerializableDataTypes;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.FallingBlockEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Pair;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.apache.commons.lang3.tuple.Triple;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,8 +23,10 @@ import java.util.UUID;
 
 public class TelekinesActions {
 	public static Map<UUID, Entity> entities = new HashMap<>();
+	public static UUID nextTaskedEntity = null;
 	public static void startAction(SerializableData.Instance data, Pair<Entity, Entity> entity) {
 		if (entity.getLeft() instanceof ServerPlayerEntity && !entities.containsValue(entity.getRight())) {
+			if(entity.getLeft().getUuid().equals(nextTaskedEntity)) nextTaskedEntity = null;
 			entities.put(entity.getLeft().getUuid(), entity.getRight());
 			entity.getRight().setInvulnerable(true);
 		}
@@ -26,7 +34,22 @@ public class TelekinesActions {
 	public static void endAction(SerializableData.Instance data, Entity entity) {
 		if(entity instanceof ServerPlayerEntity && entities.containsKey(entity.getUuid())){
 			entities.get(entity.getUuid()).setInvulnerable(false);
+			entities.get(entity.getUuid()).setNoGravity(false);
 			entities.remove(entity.getUuid());
+		}
+	}
+	public static void setTaskAction(SerializableData.Instance data, Entity entity){
+		if(entity instanceof ServerPlayerEntity) nextTaskedEntity = entity.getUuid();
+	}
+	public static void grabBlockAction(SerializableData.Instance data, Triple<World, BlockPos, Direction> stuff){
+		if(nextTaskedEntity != null && stuff.getLeft() instanceof ServerWorld world){
+			if(world.getEntity(nextTaskedEntity) instanceof ServerPlayerEntity player){
+				FallingBlockEntity fallingBlock = FallingBlockEntity.fall(world, stuff.getMiddle(), world.getBlockState(stuff.getMiddle()));
+				fallingBlock.setNoGravity(true);
+				entities.put(player.getUuid(), fallingBlock);
+				nextTaskedEntity = null;
+
+			}
 		}
 	}
 	public static void tickAction(SerializableData.Instance data, Entity entity) {
@@ -36,6 +59,18 @@ public class TelekinesActions {
 			Vec3d dest = entity.getEyePos().add(playerDir);
 			entity1.teleport(dest.x, dest.y, dest.z);
 		}
+	}
+	public static ActionFactory<Entity> getSetTaskFactory() {
+		return new ActionFactory<>(Squarepowered.id("telekinesis_task"),
+				new SerializableData(),
+				TelekinesActions::setTaskAction
+		);
+	}
+	public static ActionFactory<Triple<World, BlockPos, Direction>> getGrabBlockFactory() {
+		return new ActionFactory<>(Squarepowered.id("telekinesis_block"),
+				new SerializableData(),
+				TelekinesActions::grabBlockAction
+		);
 	}
 	public static ActionFactory<Pair<Entity, Entity>> getStartFactory() {
 		return new ActionFactory<>(Squarepowered.id("telekinesis_start"),
