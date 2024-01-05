f6d34932d1908c43b286362ac965510d12f28277
farpo <77583105+farpo@users.noreply.github.com>
2024-01-05 21:01:37 +0100

    the item geoup limitng

--- src/main/java/eu/ansquare/squarepowered/power/LimitCreativeMenuPower.java
+++ src/main/java/eu/ansquare/squarepowered/power/LimitCreativeMenuPower.java
@@ -1,35 +1,40 @@
 package eu.ansquare.squarepowered.power;
 
 import eu.ansquare.squarepowered.Squarepowered;
+import eu.ansquare.squarepowered.util.SquareDataTypes;
 import io.github.apace100.apoli.power.Power;
 import io.github.apace100.apoli.power.PowerType;
 import io.github.apace100.apoli.power.factory.PowerFactory;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.apace100.calio.data.SerializableDataTypes;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 public class LimitCreativeMenuPower extends Power {
-	public LimitCreativeMenuPower(PowerType<?> type, LivingEntity entity, List<ItemStack> stacks) {
+	public LimitCreativeMenuPower(PowerType<?> type, LivingEntity entity, List<Item> items) {
 		super(type, entity);
-		this.stacks = stacks;
+		this.items = items;
 	}
-	public final List<ItemStack> stacks;
+	public final List<Item> items;
 
 	public Collection<ItemStack> filterStacks(Collection<ItemStack> toFilter){
-		toFilter.removeAll(stacks);
+		toFilter.removeIf(stack -> items.contains(stack.getItem()));
+
+
 		return toFilter;
 	}
 	public static PowerFactory createFactory() {
 		return new PowerFactory<LimitCreativeMenuPower>(Squarepowered.id("limit_creative_menu"),
 				new SerializableData()
-						.add("removed_stacks", SerializableDataTypes.ITEM_STACKS),
+						.add("removed", SquareDataTypes.ITEMS),
 				data ->
 						(type, player) -> {
-							LimitCreativeMenuPower power = new LimitCreativeMenuPower(type, player, data.get("removed_stacks"));
+							LimitCreativeMenuPower power = new LimitCreativeMenuPower(type, player, data.get("removed"));
 							return power;
 						})
 				.allowCondition();
--- src/main/java/eu/ansquare/squarepowered/util/SquareDataTypes.java
+++ src/main/java/eu/ansquare/squarepowered/util/SquareDataTypes.java
@@ -4,12 +4,14 @@ import io.github.apace100.apoli.data.ApoliDataTypes;
 import io.github.apace100.calio.data.SerializableDataType;
 import io.github.apace100.calio.data.SerializableDataTypes;
 import net.minecraft.block.BlockState;
+import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 
 import java.util.List;
 
 public class SquareDataTypes {
 	public static final SerializableDataType<List<BlockState>> BLOCK_STATES = SerializableDataType.list(SerializableDataTypes.BLOCK_STATE);
+	public static final SerializableDataType<List<Item>> ITEMS = SerializableDataType.list(SerializableDataTypes.ITEM);
 
 
 }
