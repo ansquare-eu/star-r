9f4bba82663ea31e9fa737cdc7bd9c95170b6321
farpo <77583105+farpo@users.noreply.github.com>
2024-01-07 12:52:28 +0100

    fixed item guis

--- src/main/java/eu/ansquare/squarepowered/action/entity/OpenContainerEntityAction.java
+++ src/main/java/eu/ansquare/squarepowered/action/entity/OpenContainerEntityAction.java
@@ -8,6 +8,9 @@ import io.github.apace100.apoli.power.factory.action.ActionFactory;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.apace100.calio.data.SerializableDataTypes;
 import net.minecraft.entity.Entity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
@@ -16,14 +19,17 @@ import net.minecraft.text.Text;
 import org.quiltmc.qsl.networking.api.PacketByteBufs;
 
 public class OpenContainerEntityAction {
-	public static void action(SerializableData.Instance data, Entity entity){
-		if(entity instanceof ServerPlayerEntity player){
+	public static void action(SerializableData.Instance data, Entity entity) {
+		if (entity instanceof ServerPlayerEntity player) {
 			player.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId, playerInventory, playerx) -> {
-			GenericContainerScreenHandler handler = ScreenHandlerType.GENERIC_9X3.create(syncId, playerInventory);
-			ItemUtils.populate(handler.getInventory(), data.get("stacks"), data.getBoolean("sign") ? playerInventory.player.getUuid() : null);
-			return handler;
-		}, Text.translatable(data.getString("translation_key"))));}
+				GenericContainerScreenHandler handler = ScreenHandlerType.GENERIC_9X3.create(syncId, playerInventory);
+				Inventory inventory = handler.getInventory();
+				ItemUtils.populate(inventory, data.get("stacks"), data.getBoolean("sign") ? playerInventory.player.getUuid() : null);
+				return handler;
+			}, Text.translatable(data.getString("translation_key"))));
+		}
 	}
+
 	public static ActionFactory<Entity> getOpenActionScreenFactory() {
 		return new ActionFactory<>(Squarepowered.id("open_container_screen"),
 				new SerializableData()
