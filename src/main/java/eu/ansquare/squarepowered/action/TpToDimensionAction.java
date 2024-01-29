145725513ac380756320c42077dbcbfd4f6c85a7
farpo <77583105+farpo@users.noreply.github.com>
2024-01-29 19:41:10 +0100

    auto platforming

--- src/main/java/eu/ansquare/squarepowered/action/TpToDimensionAction.java
+++ src/main/java/eu/ansquare/squarepowered/action/TpToDimensionAction.java
@@ -1,9 +1,12 @@
 package eu.ansquare.squarepowered.action;
 
 import eu.ansquare.squarepowered.Squarepowered;
+import eu.ansquare.squarepowered.util.WorldSecurity;
 import io.github.apace100.apoli.power.factory.action.ActionFactory;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.apace100.calio.data.SerializableDataTypes;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -15,6 +18,10 @@ public class TpToDimensionAction {
 		RegistryKey<World> worldRegistryKey = data.get("dimension");
 		if(entity instanceof ServerPlayerEntity player){
 			player.teleport(player.getServer().getWorld(worldRegistryKey), data.getDouble("x"), data.getDouble("y"), data.getDouble("z"), player.getYaw(), player.getPitch());
+			if(data.getBoolean("platformIfUnsupported") && player.getServerWorld().getBlockState(player.getBlockPos().down()).isAir()){
+				BlockState state = data.get("block");
+				WorldSecurity.changeBlockState(state, player.getBlockPos().down(), player.getServerWorld(), player, true, true, false, false);
+			}
 		}
 	}
 	public static ActionFactory<Entity> getFactory(){
@@ -23,7 +30,9 @@ public class TpToDimensionAction {
 						.add("x", SerializableDataTypes.DOUBLE, 0.0d)
 						.add("y", SerializableDataTypes.DOUBLE, 0.0d)
 						.add("z", SerializableDataTypes.DOUBLE, 0.0d)
-						.add("dimension",SerializableDataTypes.DIMENSION),
+						.add("dimension",SerializableDataTypes.DIMENSION)
+						.add("platformIfUnsupported", SerializableDataTypes.BOOLEAN, false)
+						.add("block", SerializableDataTypes.BLOCK_STATE, Blocks.STONE.getDefaultState()),
 				TpToDimensionAction::action);
 	}
 }
